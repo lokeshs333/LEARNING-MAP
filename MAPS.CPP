14db0a03f55668ff2dd283eaf106ab220a6621d7
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-05-06 09:51:27 +0530

    Add files via upload

---
+++ MAPS.CPP
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+    map<char, int> first;
+
+    // initializing
+    first['a'] = 10;
+    first['b'] = 20;
+    first['c'] = 30;
+    first['d'] = 40;
+
+    map<char, int>::iterator it;
+    for (it = first.begin(); it != first.end(); ++it)
+    {
+        cout << it->first << " => " << it->second << '\n';
+    }
+
+    return 0;
+}
